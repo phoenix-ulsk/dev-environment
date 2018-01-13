d10e4983d329f51265691c30d2e8dfbea1295612
Alexander Muzyrya <muzyryaav@gmail.com>
2018-01-13 22:26:28 +0400

    [*] Add "setup" command to the script

--- dock.py
+++ dock.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
+import os
+import sys
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+HOME = os.environ["HOME"]
+
+if "DOCKER_DEV_ENVIRONMENT_DIR" in os.environ.keys():
+    DOCKER_DIR = os.environ["DOCKER_DEV_ENVIRONMENT_DIR"]
+else:
+    DOCKER_DIR = SCRIPT_DIR
 
 def printHelp():
     print("""
 List of available commands:
 
 dock help
+dock setup
 dock build
 dock start
 dock stop
@@ -23,6 +34,9 @@ Available commands explained:
 
 dock help
     Display this info
+
+doxk setup
+    Setup directories and ass this script as `dock` command to your system
 
 dock build
     Build containers
@@ -49,34 +63,67 @@ dock logs [CONTAINER IDENTIFIER]
     Get container logs
     """);
 
-def dockerPrepare():
-    os.system("mkdir -p ~/www")
-    os.system("mkdir -p ~/www/html")
-    os.system("mkdir -p ~/www/log")
-    os.system("mkdir -p ~/www/log/nginx")
-    os.system("mkdir -p ~/www/log/php")
-    os.system("mkdir -p ~/www/log/mysql5")
+def dockerSetup():
+    print("You need to super user permissions in order to execute this command")
+
+    try:
+        # Set up hosts file
+        hosts = open("/etc/hosts", "r");
+        if "dev.local" not in hosts.read():
+            os.system("""sudo echo "
+127.0.0.1\tlocalunixsocket
+127.0.0.1\tdev5.local
+127.0.0.1\tdev.local" >> /etc/hosts""")
+
+        # Add repository dir to as environment variable for future use
+        bash = open("%s/.bash_profile" % HOME, "r+");
+        if "DOCKER_DEV_ENVIRONMENT_DIR" not in bash.read():
+            bash.write("\nexport DOCKER_DEV_ENVIRONMENT_DIR=%s" % SCRIPT_DIR);
+            os.system("source ~/.bash_profile")
+
+        # Set up this script as system executable
+        if not os.path.exists("~/.dev-environment"):
+            os.system("mkdir -p ~/.dev-environment")
+            os.system("cp ./dock.py ~/.dev-environment")
+            os.system("sudo ln -s %s/.dev-environment/dock.py /usr/local/bin/dock" % HOME)
+
+        # Create directories for web server
+        if not os.path.exists("~/www"):
+            os.system("mkdir -p ~/www")
+            os.system("mkdir -p ~/www/html")
+            os.system("mkdir -p ~/www/log")
+            os.system("mkdir -p ~/www/log/nginx")
+            os.system("mkdir -p ~/www/log/php")
+            os.system("mkdir -p ~/www/log/mysql5")
+
+    except IOError as e:
+        # You need to have super user permissions to set up hosts settings
+        print(e)
+        print("You need to super user permissions in order to execute this command")
+        sys.exit(1)
 
 def dockerBuild():
-    dockerPrepare()
-    os.system("docker-compose build")
+    os.system("docker-compose -f %s/docker-compose.yml build" % DOCKER_DIR)
 
 def dockerStart():
-    os.system("docker-compose up -d")
+    os.system("docker-compose -f %s/docker-compose.yml up -d" % DOCKER_DIR)
 
 def dockerStop():
-    os.system("docker-compose stop")
+    os.system("docker-compose -f %s/docker-compose.yml stop" % DOCKER_DIR)
 
-import os
-import sys
-
-command = sys.argv[1]
+if len(sys.argv) > 1:
+    command = sys.argv[1]
+else:
+    command = "help"
 
 if command == "help":
     if (len(sys.argv) == 3 and sys.argv[2] == "all"):
         printDetailedHelp()
     else:
         printHelp()
+
+elif command == "setup":
+    dockerSetup();
 
 elif command == "build":
     dockerBuild()
