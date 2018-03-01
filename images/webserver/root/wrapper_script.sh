2649c1242f483e42b383a48a629c1cc4c63743c7
Alexander Muzyrya <muzyryaav@gmail.com>
2018-03-01 18:22:34 +0400

    [+] Add PHP 7.0 and PHP 7.2

--- images/webserver/root/wrapper_script.sh
+++ images/webserver/root/wrapper_script.sh
@@ -8,11 +8,27 @@ if [ $status -ne 0 ]; then
   exit $status
 fi
 
+# Start php 7.0 fpm daemon
+php-fpm7.0 -D
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start php fpm 7.0: $status"
+  exit $status
+fi
+
 # Start php 7.1 fpm daemon
 php-fpm7.1 -D
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start php fpm 7.1: $status"
+  exit $status
+fi
+
+# Start php 7.2 fpm daemon
+php-fpm7.2 -D
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start php fpm 7.2: $status"
   exit $status
 fi
 
@@ -31,15 +47,19 @@ fi
 # Otherwise it loops forever, waking up every 60 seconds
 
 while sleep 60; do
-  ps aux |grep php/5.6 |grep -q -v grep
+  ps aux | grep php/5.6 | grep -q -v grep
   PHP56_STATUS=$?
-  ps aux |grep php/7.1 |grep -q -v grep
+  ps aux | grep php/7.0 | grep -q -v grep
+  PHP70_STATUS=$?
+  ps aux | grep php/7.1 | grep -q -v grep
   PHP71_STATUS=$?
-  ps aux |grep nginx |grep -q -v grep
+  ps aux | grep php/7.2 | grep -q -v grep
+  PHP72_STATUS=$?
+  ps aux | grep nginx | grep -q -v grep
   NGINX_STATUS=$?
   # If the greps above find anything, they exit with 0 status
   # If they are not both 0, then something is wrong
-  if [ $PHP56_STATUS -ne 0 -o $PHP71_STATUS -ne 0 -o $NGINX_STATUS -ne 0 ]; then
+  if [ $PHP56_STATUS -ne 0 -o $PHP70_STATUS -ne 0 -o $PHP71_STATUS -ne 0 -o $PHP72_STATUS -ne 0 -o $NGINX_STATUS -ne 0 ]; then
     echo "One of the processes has already exited."
     exit -1
   fi
